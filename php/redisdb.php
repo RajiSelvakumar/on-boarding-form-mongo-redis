efd6ef422dc238ab15188696ba9f9a007c2e92c2
RajiSelvakumar <rajeswariselvakumar06@gmail.com>
2022-06-21 08:40:19 +0530

    redis

--- php/redisdb.php
+++ php/redisdb.php
@@ -5,13 +5,7 @@ try {
     $redis = new Predis\Client();
     $redis->connect('redis-13641.c92.us-east-1-3.ec2.cloud.redislabs.com:13641');
     $redis->auth(['redis', 'zbkbuY3f65W4lGZwsGdKWAvAwrkFNwyg']);
-    //$redis->connect('tls://redis-9f6095f3-of5ff6e31.database.cloud.ovh.net', 20185);
-
     
-      
-      //$redis = new Redis();
-      //$redis->connect('127.0.0.1', 6379);
-      //echo "redis connected successfully";
       
       
 } catch (Exception $e) {
