0200816aa7cf455ba2f713c319031ba6e6fcfa8b
RajiSelvakumar <rajeswariselvakumar06@gmail.com>
2022-06-21 10:34:58 +0530

    redis

--- php/redisdb.php
+++ php/redisdb.php
@@ -2,8 +2,13 @@
 require '../vendor/autoload.php';  
 //session_start();
 try {
-    $redis = new Predis\Client();
-    $redis->connect('redis-13641.c92.us-east-1-3.ec2.cloud.redislabs.com',13641);
+    $redis = new Predis\Client(array(
+        'host' => ('redis-13641.c92.us-east-1-3.ec2.cloud.redislabs.com'),
+        'port' => (13641),
+        'password' => ('zbkbuY3f65W4lGZwsGdKWAvAwrkFNwyg'),
+    ));
+    //$redis = new Predis\Client();
+    //$redis->connect('redis-13641.c92.us-east-1-3.ec2.cloud.redislabs.com',13641);
     //$redis->auth(['redis', 'zbkbuY3f65W4lGZwsGdKWAvAwrkFNwyg']);
     $redis->set("test","redis testing");
     $red = $redis->get("test");
