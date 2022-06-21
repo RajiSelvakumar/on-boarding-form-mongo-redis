bf03b406b0b28039c8539c12147b47d66a36d44d
RajiSelvakumar <rajeswariselvakumar06@gmail.com>
2022-06-21 10:37:09 +0530

    redis

--- php/redisdb.php
+++ php/redisdb.php
@@ -10,10 +10,7 @@ try {
     //$redis = new Predis\Client();
     //$redis->connect('redis-13641.c92.us-east-1-3.ec2.cloud.redislabs.com',13641);
     //$redis->auth(['redis', 'zbkbuY3f65W4lGZwsGdKWAvAwrkFNwyg']);
-    $redis->set("test","redis testing");
-    $red = $redis->get("test");
-    print_r('redis test key' . $red);
-      
+    echo "redis connection success";  
       
 } catch (Exception $e) {
     print "Error!: " . $e->getMessage() . "<br/>";
